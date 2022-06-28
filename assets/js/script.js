70a6bf7eb5ff8f4c0188f7dd02482eb3fb354560
BeckySkel <beckyskelcher@hotmail.co.uk>
2022-06-28 20:55:38 +0000

    Add javascript for main battle mechanic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,67 @@
-// main game
+// welcome message?
+
+// -- main game
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            let weapon = this.getAttribute("weapon-type");
+            battle(weapon);
+        });
+    }
+});
+
+function battle(weapon) {
+    let weapons = [{
+            value: 'rock',
+            wins: ['scissors', 'lizard'],
+            loses: ['paper', 'spock'],
+            icon: `<i class="fa-solid fa-hand-back-fist"></i>`
+        },
+        {
+            value: 'paper',
+            wins: ['spock', 'rock'],
+            loses: ['scissors', 'lizard'],
+            icon: `<i class="fa-solid fa-hand"></i>`
+        },
+        {
+            value: 'scissors',
+            wins: ['paper', 'lizard'],
+            loses: ['rock', 'spock'],
+            icon: `<i class="fa-solid fa-hand-scissors"></i>`
+        }
+        // lizard loses rock and scissors, beats paper and spock
+        // spock loses to lizard and paper, beats rock and scissors
+    ]
+
+    // -- display computer's choice
+    let compResponse = weapons[Math.floor(Math.random() * weapons.length)];
+    let compDisplay = document.getElementById('comp-display');
+    compDisplay.innerHTML = compResponse.icon;
+
+    // -- display user's choice
+    let userDisplay = document.getElementById('user-display');
+    // solution to display user selection found at https://bobbyhadz.com/blog/javascript-array-find-index-of-object-by-property
+    let weaponIndex = weapons.findIndex(object => {
+        return object.value === weapon;
+    });
+    userDisplay.innerHTML = weapons[weaponIndex].icon;
+
+    // -- display outcome 
+    let outcomeText = document.getElementById('outcome');
+    let outcome;
+    if (weapon === compResponse.loses[0] || weapon === compResponse.loses[1]) {
+        outcome = 'win';
+        outcomeText.innerHTML = `You ${outcome}!`;
+    } else if (weapon === compResponse.wins[0] || weapon === compResponse.wins[1]) {
+        outcome = 'lose'
+        outcomeText.innerHTML = `You ${outcome}!`;
+    } else {
+        outcome = 'Draw'
+        outcomeText.innerHTML = `${outcome}!`;
+    }
+}
 
 // record & display score
 
@@ -6,4 +69,4 @@
 
 // unlock new upgrade at next level?
 
-
+// theme selector?
